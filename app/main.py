77d3484397e23da80b4d1fbb80f67aa958a73935
Iasmin Vieira de Oliveira - Estagiária <iasmim.oliveira@rede.sipam.gov.br>
2025-01-03 11:22:55 -0400

    alterando a tag de biomas

--- app/main.py
+++ app/main.py
@@ -5,5 +5,5 @@ from app.api.endpoints.severity_ranking import router as severity_ranking_router
 app = FastAPI()
 
 # Adicionando o router
-app.include_router(biomas_router, tags=["Biomas"])
-app.include_router(severity_ranking_router, tags=["Ranking"])
+app.include_router(biomas_router, tags=["Eventos por Biomas"])
+app.include_router(severity_ranking_router, tags=["Ranking Severidade CIMAN"])
