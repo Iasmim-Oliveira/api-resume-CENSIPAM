60e3e562c0344f066c536d47392e5eb72ece5e73
Iasmin Vieira de Oliveira - Estagiária <iasmim.oliveira@rede.sipam.gov.br>
2025-01-06 11:24:52 -0400

    fetch :arrow_up: : criação endpoint de acumulado bioma CIMAN

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from app.api.endpoints.biomas import router as biomas_router
 from app.api.endpoints.severity_ranking import router as severity_ranking_router
 from app.api.endpoints.acumulated_br import router as acumulated_br_router
+from app.api.endpoints.acumulated_biome import router as acumulated_biome_router
+
 
 app = FastAPI()
 
@@ -9,3 +11,4 @@ app = FastAPI()
 app.include_router(biomas_router, tags=["Eventos por Biomas"])
 app.include_router(severity_ranking_router, tags=["Ranking Severidade CIMAN"])
 app.include_router(acumulated_br_router, tags=["Acumulado Brasil CIMAN"])
+app.include_router(acumulated_biome_router, tags=["Acumulado Biomas CIMAN"])
